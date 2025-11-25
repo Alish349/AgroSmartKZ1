97b2faa852a7885a1d853df4822da53a0ba985ed
Alish349 <gaslan777889@gmail.com>
2025-11-25 17:06:33 +0500

    Delete AgroSmart directory

--- AgroSmart/frontend/router.js
+++
@@ -1,8 +0,0 @@
-async function navigate(page) {
-    const html = await fetch(`pages/${page}.html`).then(r => r.text());
-    document.getElementById("app").innerHTML = html;
-
-    if (window.onPageLoad) onPageLoad(page);
-}
-
-window.addEventListener("load", () => navigate("main"));
